32ad323deee44987825d7d77242d16dbba4b42fc
Jared Kohrt <jkohrt7@gmail.com>
2021-10-27 22:52:15 -0400

    showAllEmployees and Quit choices function

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const mysql = require('mysql2');
 const EmployeeList = require('./lib/EmployeeList')
 const inquirer = require('inquirer');
+const prompts = require('./lib/prompts');
 
 //connect to db
 const db = mysql.createConnection({
@@ -11,7 +12,39 @@ const db = mysql.createConnection({
     database: 'employee_db'
 })
 
-let empDb = new EmployeeList(db);
-empDb.showAllEmployees();
-empDb.showAllDepartments();
-empDb.showAllRoles();
+
+async function init() {
+    let empDb = new EmployeeList(db);
+
+    //keep returning to start after each entry
+    var finished = false;
+    while(!finished) {
+        let choice = await prompts.selectOperationPrompt();
+        choice = choice["operation"];
+
+        switch (choice) {
+            case 'View All Employees':
+                empDb.showAllEmployees();
+                break;
+            case 'Add Employee':
+                let e = await prompts.addEmployeePrompt();
+                //empDb.addEmployee(e.employee_fname,e.employee_lname, e.employee_role, e.manager_id);
+                break;
+            case 'Update Employee Role' :
+                break;
+            case 'View All Roles' :
+                break;
+            case 'Add Department' :
+                break;
+            case 'Quit' :
+                finished = true;
+                break;
+        }
+    }
+    
+    //sever the connection to the db
+    console.log("Quitting...")
+    db.end();
+}
+
+init();
